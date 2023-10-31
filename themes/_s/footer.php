a1f950acd2cbbe8d2ddf56f2f0313010a71d04d1
Jason C <Jason@web-mo.com>
2023-10-31 16:04:03 -0700

    boilerplateCode

--- themes/_s/footer.php
+++ themes/_s/footer.php
@@ -11,27 +11,14 @@
 
 ?>
 
-	<footer id="colophon" class="footer">
+<footer id="colophon" class="footer">
 
+    <div class="row1">
+		<div class="logo">
+		<a href="/"></a><img src="/wp-content/uploads/2023/10/240_F_266840616_BX6pO1hJWgWmeQmcgKIbYSv21LD1qu6r.jpg" alt=""></a>
+		</div>
 
-	<div class="container">
-<div class="item3">
-	<a href="/"></a><img src="/wp-content/uploads/2023/10/240_F_266840616_BX6pO1hJWgWmeQmcgKIbYSv21LD1qu6r.jpg" alt=""></a>
-  </div>  
-  <div class="item1">
-	   <div class="contact">
-            <h2>Contact Us</h2>
-            <p>Email: contact@example.com</p>
-            <p>Phone: (123)456-7890</p>
-        </div>
-  </div>
-
-  <div class="item2">
-  
-  </div>
-  
-  <div class="item4">
-  <div class="links">
+       <div class="links">
             <ul>
                 <li><a href="#">Home</a></li>
                 <li><a href="#">About</a></li>
@@ -39,7 +26,25 @@
                 <li><a href="#">Contact</a></li>
             </ul>
         </div>
+    </div>
+
+
+	<div class="row2">
+        <div class="contact">
+            <h2>Contact Us</h2>
+            <p>Email: contact@example.com</p>
+            <p>Phone: (123)456-7890</p>
+        </div>
+
+    <!-- <div class="item2">
+     <p>Lorem ipsum, dolor sit amet /n
+		consectetur adipisicing elit. Quas, a?</p>
+    </div> -->
+
+  <div class="item6">
+	<div class="lorem">2023 &copy; Web-mo LLC; Call Now!</div>
   </div>
+
   <div class="item5">
 	 <div class="social">
             <h2>Follow Us</h2>
@@ -50,25 +55,9 @@
                 <li><a href="#"><i class="fa-brands fa-discord"></i></i></a></li>
             </ul>
         </div>
-	</div>
-  <div class="item6">
-	<div class="lorem">2023 &copy; Web-mo LLC; Call Now!</div>
-  </div>
 </div>
-
-
-
-
-
-		<div class="site-info">
-			<div class="footer-section">
-		
-        
-		</div>
-       
-
-
-		</div><!-- .site-info -->
+</div>
+</div>
 	</footer><!-- #colophon -->
 </div><!-- #page -->
 
